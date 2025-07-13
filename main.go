e4953d598c5905465bafb65600f3854bb79bf1aa
renatus-cartesius <cartesius.absolute@gmail.com>
2025-07-13 17:37:14 +0300

    chore: added some functionality

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,55 +12,111 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-var (
-	db *sql.DB
-)
-
-type Fuckup struct {
-	Id string `json:"id"`
-	User string `json:"user"`
-	Description string `json:"description"`
-	Likes int64 `json:"likes"`
+func init(){
+	fmt.Println("sdfsdf")
 }
 
-func getFuckups(ctx context.Context) ([]*Fuckup, error ) {
-	rows, err := db.Query("SELECT * from fuckups")
+type Fuckup struct {
+	Id    string `json:"id"`
+	User  string `json:"user"`
+	Desc  string `json:"desc"`
+	Likes int64  `json:"likes"`
+}
+
+func get(ctx context.Context, db *sql.DB) ([]*Fuckup, error) {
+	rows, err := db.QueryContext(ctx, "SELECT id, user, desc, likes FROM fuckups")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	return nil, nil 
+	fs := []*Fuckup{}
+
+	for rows.Next() {
+		f := Fuckup{}
+		err := rows.Scan(&f.Id, &f.User, &f.Desc, &f.Likes)
+
+		if err != nil {
+			return fs, err
+		}
+
+		fs = append(fs, &f)
+
+	}
+
+	return nil, nil
 }
 
-func addFuckup(ctx context.Context, f *Fuckup) error {
-	return nil
+func add(ctx context.Context, db *sql.DB, user, desc string) error {
+	stmt, err := db.PrepareContext(ctx, "INSERT INTO fuckups(user, desc) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user, desc)
+	return err
 }
 
-func likeFuckup(ctx context.Context, id string) error {
-	return nil
+func like(ctx context.Context, db *sql.DB, id string) error {
+	_, err := db.ExecContext(ctx, "UPDATE fuckups SET likes = likes + 1 WHERE id = ?", id)
+	return err
 }
 
 func main() {
 
-	var err error
+	log.Println("starting app")
 
-	db, err = sql.Open("sqlite3", "fckups.db")
+	ctx := context.Background()
+
+	log.Println("openning db")
+	db, err := sql.Open("sqlite3", "fuckups.db")
 	if err != nil {
 		log.Fatalln("error on openning db:", err)
 	}
 	defer db.Close()
 
-
 	r := gin.Default()
 
+	r.GET("/list", func(c *gin.Context) {
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		fs, err := get(ctx, db)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": fmt.Sprintf("failed when getting: %v", err),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, fs)
 	})
 
+	r.POST("/add", func(c *gin.Context) {
+
+		rb := struct{
+			User string `json:"user"`
+			Desc string `json:"desc"`
+		}{}
+
+		if err := json.NewDecoder(c.Request.Body).Decode(&rb) ; err != nil{
+			c.JSON(
+				http.StatusBadRequest,
+				gin.H{
+					"msg": "failed parse request",
+				},
+			)
+			return
+		}
+
+		if err := add(ctx, db, rb.User, rb.Desc); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": fmt.Sprintf("failed when adding: %v", err),
+			})
+			return
+		}
+	})
+
+	log.Println("running server")
 	r.Run()
 
 }
