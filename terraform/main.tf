e35cca5d70cdb8c16ee7b6382eeffcc61185ca8e
renatus-cartesius <cartesius.absolute@gmail.com>
2025-07-20 00:44:08 +0300

    chore: updated web secgroups

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,25 +47,37 @@ resource "yandex_vpc_security_group" "sg" {
     port           = 22
   }
 
-  ingress {
-    protocol       = "TCP"
-    description    = "HTTP access"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 80
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "HTTPS access"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 443
-  }
-
-  ingress {
-    protocol       = "TCP"
-    description    = "HTTP access"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    port           = 8080
+  # Allow HTTP/HTTPS only from Cloudflare IPs
+  # Cloudflare IPv4 ranges as of 2024-06 (https://www.cloudflare.com/ips/)
+  # You may want to update these periodically
+  dynamic "ingress" {
+    for_each = [
+      { port = 80,  desc = "HTTP via Cloudflare" },
+      { port = 443, desc = "HTTPS via Cloudflare" },
+      { port = 8080, desc = "HTTP (8080) via Cloudflare" }
+    ]
+    content {
+      protocol       = "TCP"
+      description    = ingress.value.desc
+      v4_cidr_blocks = [
+        "173.245.48.0/20",
+        "103.21.244.0/22",
+        "103.22.200.0/22",
+        "103.31.4.0/22",
+        "141.101.64.0/18",
+        "108.162.192.0/18",
+        "190.93.240.0/20",
+        "188.114.96.0/20",
+        "197.234.240.0/22",
+        "198.41.128.0/17",
+        "162.158.0.0/15",
+        "104.16.0.0/13",
+        "104.24.0.0/14",
+        "172.64.0.0/13",
+        "131.0.72.0/22"
+      ]
+      port = ingress.value.port
+    }
   }
 
   egress {
@@ -83,6 +95,9 @@ resource "yandex_compute_instance" "vm" {
   description = "Fuckupboard application server"
   platform_id = "standard-v3"
   zone        = "ru-central1-a"
+
+
+  allow_stopping_for_update = true
 
   # Use the cheapest instance type
   resources {
@@ -110,7 +125,7 @@ resource "yandex_compute_instance" "vm" {
   }
 
   scheduling_policy {
-    preemptible = true  # Use preemptible instances for cost savings
+    preemptible = false  # Use preemptible instances for cost savings
   }
 }
 
